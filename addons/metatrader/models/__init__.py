92a239f3fbe3f4dcb1f60edaeabfbf6f3ae9a2e3
tinh.nguyen <tinhnguyenvan91@gmail.com>
2024-12-26 10:04:09 +0700

    update addon

--- addons/metatrader/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import mt_account
-from . import mt_history
-from . import res_config_settings
--- addons/metatrader/models/res_config_settings.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class IRConfigSetting(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    metatrader_script_path = fields.Char(
-        string="MT5 trader Path", config_parameter="mt5.get_info_script_path")
